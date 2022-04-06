05e0c624ac36929577ddf0a25e9358d2a19c3920
insung3511 <insung.park123@gmail.com>
2022-04-06 11:04:01 +0900

    whats wrong with my code

--- data/median_filter.py
+++ data/median_filter.py
@@ -1,3 +1,4 @@
+from importlib.resources import path
 from scipy import signal as sp
 import pandas as pd
 import numpy as np
@@ -49,26 +50,41 @@ for i in range(len(file_dir_list)):
             db3_list = os.listdir(DATA_PATH[2])
         print("[DONE]\t\tRead file list from final_db3...")
 
-def read_csv(path_number):
-    database_csv = pd.read_csv(DATA_PATH[path_number], skipinitialspace=True)
-    return database_csv
-
 def slice_ecg_data(previous_index, current_index, input_array):
     temp_list.clear()
     for i in range(previous_index, current_index):
         temp_list.append(float(input_array[i]))
     return temp_list
 
-def median_200ms(database_csv, path_number):
+def median_200ms(path_number):
+    global database_csv_def
+    if path_number == 0:
+        for i in range(len(db1_file_list)):
+            csv_path = DATA_PATH[path_number] + db1_file_list[i]
+            database_csv_def = pd.read_csv(csv_path, skipinitialspace=True)
+            print(database_csv_def)
+    elif path_number == 1:
+        for i in range(len(db2_file_list)):
+            csv_path = DATA_PATH[path_number] + db1_file_list[i]
+            database_csv_def = pd.read_csv(csv_path, skipinitialspace=True)
+
+    elif path_number == 2:
+        for i in range(len(db2_file_list)):
+            csv_path = DATA_PATH[path_number] + db1_file_list[i]
+            database_csv_def = pd.read_csv(csv_path, skipinitialspace=True)
+
     current_index = 0
     future_index = 200
-    for i in range(0, len(database_csv)):
-        if current_index >= len(database_csv):
+    for i in range(0, len(database_csv_def)):
+        if current_index >= len(database_csv_def):
             break
         print("[INFO] >>>>>>>>> ", i, current_index, future_index)
-        print(sp.medianfilt(slice_ecg_data(current_index, future_index, database_csv)))
+        print(sp.medianfilt(slice_ecg_data(current_index, future_index, database_csv_def)))
         current_index += 200
         future_index += 200
+
+median_200ms(0)
+# median_200ms(, None)
 
 # for i in range(0, len(og_df)):
 #     if current_index >= len(og_df):
