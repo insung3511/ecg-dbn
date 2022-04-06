68096b03f9ef21e247c9e50ca5537f849513ecaf
insung3511 <insung.park123@gmail.com>
2022-04-06 23:12:39 +0900

    all done. just for now

--- data/so_again_median.py
+++ data/so_again_median.py
@@ -1,3 +1,5 @@
+import itertools
+from re import L
 from scipy import signal as sp
 import pandas as pd
 import os
@@ -19,9 +21,9 @@ result_db3_list = []
 # Slicing memory list
 temp_list = []
 
-###############################
-# Slicing ecg data set as 200ms
-###############################
+#######################
+# Slicing ecg dataset #
+#######################
 def slice_ecg_data(current_index, future_index, input_array):
     temp_list.clear()
     for i in range(current_index, future_index):
@@ -33,9 +35,9 @@ file_dir_list = os.listdir('.')
 now_index = 0
 post_index = 200
 
-####################
-# MIT-BIH Dataset 1
-####################
+########################################
+# 200ms width median MIT-BIH Dataset 1 #
+########################################
 FILE_FLAG_NUMBER = 0
 print("[INFO]\tfinal_db1 direcotry found.")
 db1_file_list = os.listdir(DATA_PATH[FILE_FLAG_NUMBER])
@@ -52,9 +54,9 @@ for i in range(len(db1_file_list)):
     now_index += 200
     post_index += 200
 
-####################
-# MIT-BIH Dataset 2
-####################
+########################################
+# 200ms width median MIT-BIH Dataset 2 #
+########################################
 FILE_FLAG_NUMBER = 1
 print("[INFO]\tfinal_db2 direcotry found.")
 db2_file_list = os.listdir(DATA_PATH[FILE_FLAG_NUMBER])
@@ -64,8 +66,65 @@ for i in range(len(db2_file_list)):
     db2_csv = pd.read_csv(read_csv_path)
     file_name_check = os.path.splitext(db2_file_list[i])[0]
 
-    mlii_list = list(db2_csv["'MLII'"])
-    print("[IWIP]\tfinal_db2 reading...", i, now_index, post_index)
-    result_db2_list.append(list(sp.medfilt(slice_ecg_data(now_index, post_index, mlii_list))))
+    try:
+        mlii_list = list(db2_csv["'MLII'"])
+        print("[IWIP]\tfinal_db2 reading...", i, now_index, post_index)
+        result_db2_list.append(list(sp.medfilt(slice_ecg_data(now_index, post_index, mlii_list))))
+    except KeyError:
+        continue
+
     now_index += 200
     post_index += 200
+
+#####################################
+# 200ms width median SVDB Dataset 3 #
+#####################################
+FILE_FLAG_NUMBER = 2
+print("[INFO]\tfinal_db3 direcotry found.")
+db3_file_list = os.listdir(DATA_PATH[FILE_FLAG_NUMBER])
+print("....\t...................i Current_Index From_Index")
+for i in range(len(db2_file_list)):
+    read_csv_path = DATA_PATH[FILE_FLAG_NUMBER] + db3_file_list[i]
+    db3_csv = pd.read_csv(read_csv_path)
+    file_name_check = os.path.splitext(db3_file_list[i])[0]
+
+    try:
+        ecg_list = list(db3_csv["'ECG1'"])
+        print("[IWIP]\tfinal_db3 reading...", i, now_index, post_index)
+        result_db3_list.append(list(sp.medfilt(slice_ecg_data(now_index, post_index, ecg_list))))
+    except KeyError:
+        continue
+    
+    now_index += 200
+    post_index += 200
+
+'''Okay,
+200ms-width-median filtering is over...maybe.
+And now we going to do 600ms-width median filtering from 200ms-width median filter'''
+
+#######################
+# Combine in one list #
+#######################
+combine_list_in_list = result_db1_list + result_db2_list + result_db3_list
+combine_list = list(itertools.chain(*combine_list_in_list))
+print(combine_list)
+
+final_result = []
+now_index = 0
+post_index = 600
+
+print("[INFO] BEGIN OF 600ms width median filtering")
+FILE_FLAG_NUMBER = 0
+
+print("......\t...................i\tCurrent_Index\tFrom_Index")
+
+# print(all_200ms_list)
+# mlii_list = list(db1_csv["'MLII'"])
+for i in range(len(combine_list)):
+    if len(combine_list) <= post_index: break
+    print("[IWIP]\tfinal all list reading...", i, now_index, post_index)
+    final_result.append((sp.medfilt(slice_ecg_data(now_index, post_index, combine_list))))
+    now_index += 600
+    post_index += 600
+
+print("[DONE] AHHHHHHHHHHHHHHHHHHHHHHHHHHH FUCK")
