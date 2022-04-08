9ee69b772c4227047fdfb90bb6a28a677a886331
insung3511 <insung.park123@gmail.com>
2022-04-08 10:16:25 +0900

    GB-DBN is f*&^ed up

--- GB-DBN.py
+++ GB-DBN.py
@@ -1,3 +1,5 @@
+# reference from : https://github.com/meownoid/tensorflow-rbm
+
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
@@ -15,6 +17,14 @@ class GB_RBM():
         self.hidden = nn.Parameter(torch.randn(1, HIDDEN_UNITS))
         self.weight = nn.Parameter(torch.randn(HIDDEN_UNITS, VISIBLE_UNITS))
         self.k = K_FOLD
+        super().__init__(VISIBLE_UNITS, HIDDEN_UNITS)
+
+    def step(self, x):
+        hidden_p = nn.sigmoid(torch.matmul(x) + self.hidden)
+        weight_mut_hidden = F.linear(self.visible, self.weight) + self.hidden)
+
+        if self.visible:
+            visible = nn.sigmoid(nn.transpose(x), hidden_p)
 
     def sigmoid(self, x):
         return nn.Sigmoid(x)
