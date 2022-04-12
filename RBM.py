8e28e8fbcf018c9853e20919f7413f3516c785ba
insung3511 <insung.park123@gmail.com>
2022-04-13 02:00:54 +0900

    다 몰라요. 햄버거 사주세요

---
+++ RBM.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6863c6a696213f911bfe42e054201c2efba1d942bd690615c8f6144c9370fb46
+size 4263
--- model.py
+++ model.py
@@ -1,36 +1,35 @@
 import data.medain_filtering_class as mf
-from numpy import float64
-import tensorflow as tf
+import torch.nn as nn
 import numpy as np
+from RBM import *
 import logging
+import torch
 
+BATCH_SIZE = 10
+EPOCH = 100
+LEARNING_RATE = 0.2
+ANNEALING_RATE = 0.999
+VISIBLE_UNITS = 180
+HIDDEN_UNITS = 80
 
+print("[MODL] Model main code is starting....")
+logging.basicConfig(level=logging.INFO)
 
-'''
-####################
-####################
-####################
-####################
-####################
-####################
-####################
-####################
-####################
-####################
-'''
-if __name__ == '__main__':
-    BATCH_SIZE = 10
-    EPOCH = 100
-    LEARNING_RATE = 0.2
-    ANNEALING_RATE = 0.999
-    
-    print("[MODL] Model main code is starting....")
-    logging.basicConfig(level=logging.INFO)
+print("[INFO] Read train data, cross-vaildation data and test data from median filtering code")
+dataset_db1, dataset_db2, dataset_db3 = mf.ecg_filtering(True)
 
-    print("[INFO] Read train data, cross-vaildation data and test data from median filtering code")
-    dataset_db1, dataset_db2, dataset_db3 = mf.ecg_filtering(True)
-    train_dataset = tuple(mf.list_to_list(dataset_db1 + dataset_db2))
-    test_dataset = tuple(mf.list_to_list(dataset_db2 + dataset_db3))
+train_dataset = list(mf.list_to_list(dataset_db1 + dataset_db2))
+test_dataset = list(mf.list_to_list(dataset_db2 + dataset_db3))
+train_data = torch.Tensor(train_dataset)
+test_data = torch.Tensor(test_dataset)
 
-    train_data = np.array(train_dataset)
-   
+print(type(train_data))
+print(type(test_data))
+
+bbrbm = RBMBer(VISIBLE_UNITS, HIDDEN_UNITS)
+m = nn.AdaptiveAvgPool1d(80)
+train_data = m(train_data)
+
+for _ in range(BATCH_SIZE):
+    error = bbrbm.cd(train_data)
+    print("Reconstruction loss : %.3f" % (error.data[0]))
