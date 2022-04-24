d6fbfaa207649963ce225e6c38a87fe2bfd750a3
insung3511 <insung.park123@gmail.com>
2022-04-24 15:57:22 +0900

    Line 78, problem solved

--- RBM.py
+++ RBM.py
@@ -1,3 +1,110 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:755287d61c516992389dfec154d559f415499d721f31ed8894eeac3906bc8ee0
-size 3426
+"""
+PyTorch implementation of all (Bernoulli, Gaussian hid, Gaussian vis+hid) kinds of RBMs.
+
+(C) Kai Xu
+University of Edinburgh, 2017 
+
+Reference from https://github.com/xukai92/pytorch-rbm/blob/ea88786dc8352dae59a4e306ad8fe4d274e13c14/rbm.py
+"""
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import numpy as np
+
+class RBMBase:
+    def __init__(self, vis_num, hid_num):
+        self.vis_num = vis_num
+        self.hid_num = hid_num
+
+        # Dictionary for storing parameters
+        self.params = dict()
+
+        # self.w = torch.randn(vis_num, hid_num) * 0.1    # weight matrix
+        self.w = nn.Parameter(torch.randn(hid_num, vis_num) * 0.1)
+        self.a = torch.zeros(vis_num) / vis_num         # bias for visiable units
+        self.b = torch.zeros(hid_num)                   # bias for hidden units
+        
+        # Corresponding momentums; _v means velocity of
+        self.w_v = torch.zeros(vis_num, hid_num)
+        self.a_v = torch.zeros(vis_num)
+        self.b_v = torch.zeros(hid_num)
+    
+        self.has_gpu = False
+            
+    def p_h_given_v(self, v):
+        raise NotImplementedError()
+
+    def sample_h_given_v(self, v):
+        raise NotImplementedError()
+
+    def p_v_given_h(self, h):
+        raise NotImplementedError()
+
+    def cd(self, v_data, k, eta, alpha, lam):
+        """
+        Perform contrastive divergence with k stpes, i.e. CD_k.
+
+        @input
+            v_data: visible data
+                 k: MCMC step number
+               eta: learning rate
+             alpha: momentum coefficient
+               lam: weight decay rate
+
+        @return
+            error: reconstruction error
+        """
+
+        # Positive phase
+    #   tensor, tensor(but change it to src)
+        h_pos, h_prob_pos = self.sample_h_given_v(v_data)
+        # Negative phase
+        h_neg = h_pos.clone()
+
+        for _ in range(k):
+            v_prob_neg = self.p_v_given_h(h_neg)
+            h_neg, h_prob_neg = self.sample_h_given_v(v_prob_neg)
+
+        # Compute statistics
+                #   tensor            matrix
+        # print(type(h_prob_pos), type(v_data))
+        # print(h_prob_pos, v_data)
+        #------------------------------------------INPUT---------DIM
+        #                     # Size  S  E
+        index_tensor = torch.Tensor.long(torch.ones(self.vis_num, 0))
+        v_data = v_data.unsqueeze(1)
+        h_prob_pos = h_prob_pos.unsqueeze(0)
+
+        print(h_prob_pos.size(), index_tensor.size(), v_data.size())
+        print(torch.numel(h_prob_pos), torch.numel(v_data))
+        # print(v_data)
+        # print(h_prob_pos)
+
+        # Okay solved.
+        stats_pos = torch.matmul(
+            (v_data).scatter_(0, index_tensor, v_data.clone()), 
+            h_prob_pos
+        )
+
+        # And other issue part
+        stats_neg = torch.matmul(v_prob_neg.t(), h_prob_neg)
+        
+        # Compute gradients
+        w_grad = (stats_pos - stats_neg) / batch_size
+        batch_size = v_data.size()[0]
+        a_grad = torch.sum(v_data - v_prob_neg, 0) / batch_size
+        b_grad = torch.sum(h_prob_pos - h_prob_neg, 0) / batch_size
+
+        # Update momentums
+        self.w_v = alpha * self.w_v + eta * (w_grad - lam * self.w)
+        self.a_v = alpha * self.a_v + eta * a_grad
+        self.b_v = alpha * self.b_v + eta * b_grad
+
+        # Update parameters
+        self.w = self.w + self.w_v
+        self.a = self.a + self.a_v
+        self.b = self.b + self.b_v
+
+        # Compute reconstruction error
+        error = F.mse_loss(v_data, v_prob_neg, size_average=False)
+        return error
