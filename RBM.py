839775d51ab45913ce337d579cd5c058d27fa1ec
insung3511 <insung.park123@gmail.com>
2022-05-11 16:39:02 +0900

    Still testing...

--- RBM.py
+++ RBM.py
@@ -8,7 +8,7 @@ import numpy as np
 import timeit
 import torch
 
-SIZE = 10
+SIZE = 13000000
 
 class RBM(nn.Module):
     global SIZE
@@ -83,7 +83,7 @@ class RBM(nn.Module):
 class new_RBM(nn.Module): 
     global SIZE
     def __init__(self, n_vis, n_hid, k, batch):
-        super(RBM, self).__init__()
+        super(new_RBM, self).__init__()
         self.W      = nn.Parameter(torch.randn(1, batch) * 1e-2)
         self.v_bias = nn.Parameter(torch.zeros(batch))
         self.h_bias = nn.Parameter(torch.zeros(batch))
@@ -99,27 +99,24 @@ class new_RBM(nn.Module):
 
     #                v is input data from visible layer
     def v_to_h(self, v):
-        h_bias = (self.h_bias.clone()).expand(SIZE)
-        # v = v.clone().repeat(130000)
-        w = self.W.clone().repeat(SIZE, 1)
+        print(type(v.size()))
+        h_bias = self.h_bias.clone().repeat(v.size())
+        w = self.W.clone()
 
         p_h = F.sigmoid(
             F.linear(v, w, bias=h_bias)
         )
 
-        # p_h = F.sigmoid(
-        #     F.linear(v, torch.flatten(self.W), self.h_bias)
-        # )        
         sample_h = self.sample_from_p(p_h)
         return p_h, sample_h
 
     def h_to_v(self, h):
-        v_bias = (self.v_bias.clone())
-        w = self.W.t().clone().repeat(1, SIZE)
+        w = self.W.t().clone()
 
         p_v = F.sigmoid(
             F.linear(h, w, self.v_bias)
         )
+        
         sample_v = self.sample_from_p(p_v)
         return p_v, sample_v
     
@@ -135,14 +132,10 @@ class new_RBM(nn.Module):
         return v, v_, estimate_time
 
     def free_energy(self, v):
-        v = v.clone().unsqueeze(1).repeat(1, SIZE)
-        v_bias = self.v_bias.clone()
         v_bias_term = torch.mv(v, self.v_bias)
-
         h_bias = self.h_bias.clone()
-        w = self.W.clone().repeat(SIZE, 1)
         
-        wx_b = F.linear(v, w, h_bias)
+        wx_b = F.linear(v, self.W, h_bias)
         hidden_term = wx_b.exp().add(1).log().sum(1)
         
         return (-(hidden_term) - v_bias_term).mean()
