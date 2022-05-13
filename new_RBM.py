0c4443cc063db65b6a80e3d1a11caeff82b589f0
unknown <insung.park123@gmail.com>
2022-05-13 19:47:35 +0900

    Changed loss

--- new_RBM.py
+++ new_RBM.py
@@ -3,29 +3,27 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.utils.data
 import torch.nn as nn
+import logging
 import torch
 
-class new_RBM(nn.Module):
+class new_RBM(nn.Module): 
+    global SIZE
     def __init__(self, n_vis, n_hid, k, batch):
         super(new_RBM, self).__init__()
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        device = torch.device(device)
-
         self.W      = nn.Parameter(torch.randn(1, batch) * 1e-2)
         self.n_vis  = n_vis
         self.n_hid  = n_hid
         self.k      = k
         self.batch  = batch
-        self.v_bias = nn.Parameter(torch.zeros(batch, device=device))
-        self.h_bias = nn.Parameter(torch.zeros(batch, device=device))
-        self.device = device
+        self.v_bias = nn.Parameter(torch.zeros(batch))
+        self.h_bias = nn.Parameter(torch.zeros(batch))
     
     def sample_from_p(self, p):
         return F.relu(
             torch.sign(
-                p - Variable(torch.randn(p.size(), device=self.device))
+                p - Variable(torch.randn(p.size()))
             )
-        ).to(self.device)
+        )
 
     ''' ISSUE PART '''
     def v_to_h(self, v):
@@ -33,7 +31,7 @@ class new_RBM(nn.Module):
 
         p_h = F.sigmoid(
             F.linear(v, w)
-        ).to(self.device)
+        )
 
         sample_h = self.sample_from_p(p_h)
         return p_h, sample_h
@@ -43,31 +41,29 @@ class new_RBM(nn.Module):
 
         p_v = F.sigmoid(
             F.linear(h, w)
-        ).to(self.device)
+        )
         
         sample_v = self.sample_from_p(p_v)
         return p_v, sample_v
     
     def forward(self, v):
         start_time = datetime.now()
-        
         pre_h1, h1 = self.v_to_h(v)
         h_ = h1
 
         for _ in range(self.k):
             pre_v_, v_ = self.h_to_v(h_)
             pre_h_, h_ = self.v_to_h(v_)
-
         estimate_time = datetime.now() - start_time
         return v, v_, estimate_time
 
-    def free_energy(self, v):
-        v_bias_term = torch.mv(v.view(1300000, self.batch), self.v_bias.view(self.batch, 1)).to(device=self.device)
+    # def free_energy(self, v):
+    #     v_bias_term = torch.mv(v.view(-1, 10), self.v_bias)
 
-        wx_b = F.linear(v, self.W)
-        hidden_term = wx_b.unsqueeze(0).exp().add(1).log().sum(1)
+    #     wx_b = F.linear(v, self.W)
+    #     hidden_term = wx_b.unsqueeze(0).exp().add(1).log().sum(1)
         
-        return (-(hidden_term) - v_bias_term).mean()
+    #     return (-(hidden_term) - v_bias_term).mean()
     
     def get_weight(self):
         return self.W
